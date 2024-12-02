f8b753a43df2ea945ac5ba39c80e193f0e0c49fc
Yoram Hernalsteen <y.hernalsteen@xpofleet.com>
2024-12-02 12:31:14 +0100

    use on windows and mac

--- AdventOfCode24/Core.cs
+++ AdventOfCode24/Core.cs
@@ -2,11 +2,11 @@ namespace AdventOfCode24;
 
 public static class Core
 {
-    private static readonly string _directory = "/Users/yoramhernalsteen/RiderProjects/AdventOfCode24/AdventOfCode24/Input";
     
     public static List<string> ConvertFileToLines(string filename)
     {
-        var file = Path.Combine(_directory, filename);
+        var directory = DotNetEnv.Env.GetString("INPUT_DIR");
+        var file = Path.Combine(directory, filename);
         file += ".txt";
         var lines = File.ReadAllLines(file);
         return lines.ToList();
--- AdventOfCode24/Program.cs
+++ AdventOfCode24/Program.cs
@@ -6,6 +6,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        var loaded = DotNetEnv.Env.Load();
+
         Console.WriteLine($"EASY: {DayOne.Solve()}");
         Console.WriteLine($"EXTRA: {DayOne.SolveExtra()}");
     }
